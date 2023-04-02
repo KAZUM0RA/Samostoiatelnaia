95abfa56277e70f39975238dc64512199648e057
KAZUM0RA <124499055+KAZUM0RA@users.noreply.github.com>
2023-04-02 12:22:09 +0300

    1

---
+++ Ur10/Actions.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace Ur10 {
+    public static class FileUtils {
+        public static string ReadStringFromFile(string fileName) {
+            using (StreamReader reader = new StreamReader(fileName)) {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static void WriteStringToFile(string fileName, string contents) {
+            using (StreamWriter writer = new StreamWriter(fileName)) {
+                writer.WriteLine(contents);
+            }
+        }
+    }
+}
---
+++ Ur10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ur10;
+
+Console.WriteLine("Введите строку:");
+string input = Console.ReadLine();
+
+string trimmed = input.Trim();
+
+string name = "Aleks";
+string replaced = trimmed.Replace("%name%", name);
+
+FileUtils.WriteStringToFile("profile.txt", replaced);
+
+string contents = FileUtils.ReadStringFromFile("profile.txt");
+Console.WriteLine(contents);
+
+string template = FileUtils.ReadStringFromFile("template.txt");
+string result = template.Replace("???", replaced);
+
+FileUtils.WriteStringToFile("newFile.txt", result);
+
+Console.WriteLine("Готово, я спать");
